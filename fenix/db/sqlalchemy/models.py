ac9fe415e63d9cf9b8d3069df3f7d2677e4ccf8d
Tomi Juvonen <tomi.juvonen@nokia.com>
2019-03-26 15:20:13 +0200

    Bug: host type should be nullable
    Host type needs to be nullable as it can be given as list where
    the type is yet not known.
    
    Change-Id: Ie332281d3050e58fba9ae40cfb5e52b624c49cc5
    Signed-off-by: Tomi Juvonen <tomi.juvonen@nokia.com>

--- fenix/db/sqlalchemy/models.py
+++ fenix/db/sqlalchemy/models.py
@@ -94,7 +94,7 @@ class MaintenanceHost(mb.FenixBase):
     session_id = sa.Column(sa.String(36), sa.ForeignKey('sessions.session_id'),
                            nullable=False)
     hostname = sa.Column(sa.String(length=255), primary_key=True)
-    type = sa.Column(sa.String(length=32), nullable=False)
+    type = sa.Column(sa.String(length=32), nullable=True)
     maintained = sa.Column(sa.Boolean, default=False)
     disabled = sa.Column(sa.Boolean, default=False)
     details = sa.Column(sa.String(length=255), nullable=True)
